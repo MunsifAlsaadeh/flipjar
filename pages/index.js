f54016907b3bb8b30ec909d21cb870c1aff1d070
MunsifAlsaadeh <monsef84@hotmail.com>
2025-06-17 20:32:02 +0300

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold">Welcome to Flipjar</h1>
+    </div>
+  );
+}
