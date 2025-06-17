26f4550350afec5771d7e5e6d0343240ce9bcdb6
MunsifAlsaadeh <monsef84@hotmail.com>
2025-06-17 20:27:59 +0300

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="flex justify-center items-center h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold">Welcome to Flipjar</h1>
-    </div>
-  );
-}
